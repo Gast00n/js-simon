d0cbb893daf978cac38aa0f473309bde723d51b0
Marco Giulietti <ga6t00n@gmail.com>
2020-10-29 17:46:15 +0100

    Done!

--- js/main.js
+++ js/main.js
@@ -14,10 +14,10 @@
 
     //References
 
-     var btn = $('.btn');
      var bar = $('.bar');
      var timer = $('.timer');
      var boxes = $('.boxes');
+     var results = $('.result-text');
     
 
     //setup 
@@ -35,7 +35,7 @@
 
     //  2. Gestione del counter di 30 Secondi
     var width = 100;
-    var secs = 30;
+    var secs = 10;
     var timeUp = setInterval(function() {
         if(secs == 0) {
             bar.css('width', width + '%');
@@ -43,9 +43,41 @@
             var numBox = $('.boxes > li.box');
             numBox.html('<span class="number"><i class="fas fa-question"></i></span>');
             numBox.css('color', 'tomato');
+
+            //  3. Prompt Inserimento Numeri
+            var insert = [];
+            var output = '';
+            var corretti = 0;
+            var sbagliati = 0;
+            for ( i = 0; i < 5; i++) {
+                var userNum = parseInt( prompt('Inserisci un numero compreso tra 1 e 100!') );
+                while (isNaN(userNum) || userNum < 1 || userNum > 100 || insert.includes(userNum)) {
+                    userNum = parseInt( prompt('Inserisci un numero, VALIDO, compreso tra 1 e 100!') );
+                }
+                insert.push(userNum);
+
+                if (! numbers.includes(userNum)) {
+                    output += '<span class="red">' + userNum + '</span>, '
+                    sbagliati++;
+                } else {
+                    output += '<span class="green">' + userNum + '</span>, '
+                    corretti++;
+                }
+            }
+
+            console.log(output + corretti + ' ' + sbagliati);
+
+            if (corretti == 5) {
+                results.html('COMPLIMENTI! Hai vinto! Hai ricordato tutti e 5 i numeri!<br>La sequenza originale è: <strong>' + numbers + '</strong><br>I numeri inseriti sono:' + output);
+            } else if  (sbagliati == 5)  {
+                results.html('DANNAZIONE! Non hai una gran memoria, prova a ricaricare la pagina!<br>La sequenza originale è: <strong>' + numbers + '</strong><br> I numeri inseriti sono:' + output);
+            } else {
+                results.html('Hai indovinato ' + corretti + ' numeri. Ne hai però sbagliati: ' + sbagliati + '<br>La sequenza originale è: <strong>' + numbers + '</strong><br>I numeri inseriti sono:' + output);
+            }
+
             clearInterval(timeUp);
         } else {
-            width = width - (100/30);
+            width = width - (100/10);
             bar.css('width', width + '%');
             timer.text(secs);
             secs--;
